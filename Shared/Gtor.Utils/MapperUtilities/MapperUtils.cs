7031f1174d87c453d0cfa1ed03acafff63b3b77f
wcarpiom <wcarpio@prosoftnearshore.com>
2017-09-28 17:17:40 -0500

    Add MapperUtils class

---
+++ Shared/Gtor.Utils/MapperUtilities/MapperUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gtor.Utils.MapperUtilities
+{
+    public interface IMapperUtils
+    {
+        string GetStringTypeByType(Type dataType);
+    }
+    public class MapperUtils : IMapperUtils
+    {
+        private static readonly Dictionary<Type, string> DataTypeRepo = new Dictionary<Type, string>
+        {
+            [typeof(byte)] = "byte",
+            [typeof(sbyte)] = "sbyte",
+            [typeof(short)] = "int16",
+            [typeof(ushort)] = "ushort",
+            [typeof(int)] = "int",
+            [typeof(uint)] = "uint",
+            [typeof(long)] = "long",
+            [typeof(ulong)] = "ulong",
+            [typeof(float)] = "float",
+            [typeof(double)] = "double",
+            [typeof(decimal)] = "decimal",
+            [typeof(bool)] = "bool",
+            [typeof(string)] = "string",
+            [typeof(char)] = "char",
+            [typeof(Guid)] = "Guid",
+            [typeof(DateTime)] = "DateTime",
+            [typeof(DateTimeOffset)] = "DateTime",
+            [typeof(byte[])] = "byte[]"
+        };
+
+        public string GetStringTypeByType(Type dataType)
+        {
+            if (!DataTypeRepo.ContainsKey(dataType))
+            {
+                throw new ArgumentException($"Type \"{dataType}\" not recognized as valid data type.");
+            }
+            return DataTypeRepo[dataType];
+        }
+    }
+}
