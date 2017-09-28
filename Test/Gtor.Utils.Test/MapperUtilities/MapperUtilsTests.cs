4afd8861760ee7849f9bfca302c6a13c5974a11d
wcarpiom <wcarpio@prosoftnearshore.com>
2017-09-28 17:21:36 -0500

    Add Test for MapperUtils

---
+++ Test/Gtor.Utils.Test/MapperUtilities/MapperUtilsTests.cs
@@ -0,0 +1,31 @@
+﻿using Gtor.Utils.MapperUtilities;
+using NUnit.Framework;
+using System;
+
+namespace Gtor.Utils.Test.MapperUtilities
+{
+    [TestFixture(TestOf = typeof(MapperUtils)), Category("Unit")]
+
+    public class MapperUtilsTests
+    {
+        private IMapperUtils _target;
+        private Type _stringTypeTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _target = new MapperUtils();
+            _stringTypeTest = typeof(int);
+        }
+
+        [Test]
+        public void Test_GetTypeByFriendlyName()
+        {
+            var expected = "int";
+
+            var result = _target.GetStringTypeByType(_stringTypeTest);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
